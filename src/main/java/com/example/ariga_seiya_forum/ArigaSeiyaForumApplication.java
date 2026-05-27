79152f13b9ed83d0f5b78e1d06118dfad28f5ce0
ariga_seiya <ariga.seiya@alhinc.jp>
2026-05-27 13:28:52 +0900

    first commit

---
+++ src/main/java/com/example/ariga_seiya_forum/ArigaSeiyaForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.ariga_seiya_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArigaSeiyaForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArigaSeiyaForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ariga_seiya_forum/ArigaSeiyaForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ariga_seiya_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArigaSeiyaForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
